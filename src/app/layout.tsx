27c4e19ebe885df28a824564c5416a47d85ae567
Zeyu Yao <novodoodle@gmail.com>
2025-12-26 18:20:26 +0800

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 const nunito = Nunito({
   variable: "--font-nunito",
@@ -49,6 +50,7 @@ export default function RootLayout({
         className={`${nunito.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
